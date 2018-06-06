271cb906de3c852f56b8bab830757bdd872f0aa7
joshpson <jpearson846@gmail.com>
2018-06-06 14:43:35 -0400

    Done.

--- konami_code.js
+++ konami_code.js
@@ -12,5 +12,25 @@ const codes = [
 ];
 
 function init() {
-  // your code here
+  document.body.addEventListener('keydown', function(event) {
+    checkCode(event.key);
+  })
 }
+
+let index = 0;
+
+function checkCode(key) {
+  if (key != codes[index]) {
+    index = 0;
+    console.log(key, index);
+  } else {
+    index += 1;
+    if (index === 10) {
+      console.log("YOU DID IT", key, index);
+      alert("Hurray!");
+    } else {
+      console.log(key, index);
+    }
+  }
+}
+
