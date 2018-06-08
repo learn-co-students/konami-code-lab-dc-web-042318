0e354c1fbfeafff8b357d7d2de4fcfc5a48725be
github username <github email address>
2018-06-08 14:52:09 -0400

    Done.

--- konami_code.js
+++ konami_code.js
@@ -11,6 +11,18 @@ const codes = [
   "a"
 ];
 
+
 function init() {
-  // your code here
-}
+  i = 0
+  document.body.addEventListener('keydown', function(e) {
+    const key = e.key;
+    if (key === codes[i]) {
+      i++;
+    } else {
+      i = 0;
+    }
+    if (i === codes.length) {
+      window.alert('Nailed It!');
+    }
+  })
+};
