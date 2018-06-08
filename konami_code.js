0aa48e8105113ae3fd2e1a33f6d9516cbb7a854c
rkim91 <richeekim@gmail.com>
2018-06-08 14:38:42 -0400

    Done.

--- konami_code.js
+++ konami_code.js
@@ -12,5 +12,21 @@ const codes = [
 ];
 
 function init() {
-  // your code here
+  let index = 0;
+
+  document.body.addEventListener("keydown", (e) => {
+    const key = e.key;
+
+    if (codes[index] === key) {
+      index++;
+
+      if (index === codes.length) {
+        alert("Hurray!");
+
+        index = 0;
+      }
+    } else {
+      index = 0;
+    }
+  });
 }
